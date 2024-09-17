6c39985eaa51a8286857959408c9ea68f92a14c0
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-09-17 14:59:45 +0200

    Update code

--- src/linked_list/LinkedList.java
+++ src/linked_list/LinkedList.java
@@ -1,10 +1,10 @@
 package linked_list;
 
-
 public class LinkedList {
 
     private Node head;
     private Node tail;
+    private int length;
 
     class Node {
         int value;
@@ -19,6 +19,7 @@ public class LinkedList {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
+        length = 1;
     }
 
     public Node getHead() {
@@ -27,6 +28,10 @@ public class LinkedList {
 
     public Node getTail() {
         return tail;
+    }
+
+    public int getLength() {
+        return length;
     }
 
     public void printList() {
@@ -38,15 +43,16 @@ public class LinkedList {
     }
 
     public void printAll() {
-        if (head == null) {
+        if (length == 0) {
             System.out.println("Head: null");
             System.out.println("Tail: null");
         } else {
             System.out.println("Head: " + head.value);
             System.out.println("Tail: " + tail.value);
         }
+        System.out.println("Length:" + length);
         System.out.println("\nLinked List:");
-        if (head == null) {
+        if (length == 0) {
             System.out.println("empty");
         } else {
             printList();
@@ -56,26 +62,127 @@ public class LinkedList {
     public void makeEmpty() {
         head = null;
         tail = null;
+        length = 0;
     }
 
     public void append(int value) {
         Node newNode = new Node(value);
-        if (head == null) {
+        if (length == 0) {
             head = newNode;
             tail = newNode;
         } else {
             tail.next = newNode;
             tail = newNode;
         }
+        length++;
     }
 
-    // WRITE FIND MIDDLE NODE METHOD HERE //
-    public Node findMiddleNode(){
-
-
-        return null;
+    public Node removeLast() {
+        if (length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while (temp.next != null) {
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if (length == 0) {
+            head = null;
+            tail = null;
+        }
+        return temp;
     }
 
+    public void prepend(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        length++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) return null;
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length) return null;
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) return null;
+        if (index == 0) return removeFirst();
+        if (index == length - 1) return removeLast();
+
+        Node prev = get(index - 1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+    // WRITE REVERSE METHOD HERE //
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+
+
+    }
 }
-
-
